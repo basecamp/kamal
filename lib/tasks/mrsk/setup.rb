fdfdff65e91af88b23554e5e01bba18f13923e50
David Heinemeier Hansson <david@loudthinking.com>
2023-01-08 16:33:09 +0100

    Explain command map overwrite

--- lib/tasks/mrsk/setup.rb
+++ lib/tasks/mrsk/setup.rb
@@ -7,4 +7,5 @@ MRSK_CONFIG = Mrsk::Configuration.load_file(Rails.root.join("config/deploy.yml")
 
 SSHKit::Backend::Netssh.configure { |ssh| ssh.ssh_options = MRSK_CONFIG.ssh_options }
 
+# No need to use /usr/bin/env, just clogs up the logs
 SSHKit.config.command_map[:docker] = "docker"
