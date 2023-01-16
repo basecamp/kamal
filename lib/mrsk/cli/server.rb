cb26fb9dcaa56c4c7ea8ee286df25b4f7a93255b
David Heinemeier Hansson <david@loudthinking.com>
2023-01-16 19:06:00 +0100

    Run update as well before install (as some servers dont have it available otherwise)

--- lib/mrsk/cli/server.rb
+++ lib/mrsk/cli/server.rb
@@ -3,6 +3,6 @@ require "mrsk/cli/base"
 class Mrsk::Cli::Server < Mrsk::Cli::Base
   desc "bootstrap", "Ensure Docker is installed on the servers"
   def bootstrap
-    on(MRSK.config.hosts) { execute "which docker || apt-get install docker.io -y" }
+    on(MRSK.config.hosts) { execute "which docker || (apt-get update -y && apt-get install docker.io -y)" }
   end
 end
