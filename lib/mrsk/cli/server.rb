f0d88a5ffe6b27eb7abef8f8e2228700692a782e
David Heinemeier Hansson <david@loudthinking.com>
2023-01-23 14:13:10 +0100

    Bootstrap accessory hosts too

--- lib/mrsk/cli/server.rb
+++ lib/mrsk/cli/server.rb
@@ -3,6 +3,6 @@ require "mrsk/cli/base"
 class Mrsk::Cli::Server < Mrsk::Cli::Base
   desc "bootstrap", "Ensure Docker is installed on the servers"
   def bootstrap
-    on(MRSK.hosts) { execute "which docker || (apt-get update -y && apt-get install docker.io -y)" }
+    on(MRSK.hosts + MRSK.accessory_hosts) { execute "which docker || (apt-get update -y && apt-get install docker.io -y)" }
   end
 end
