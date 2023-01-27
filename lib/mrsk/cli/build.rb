f58e5e0935bd975e2fb27a5d243ec500db1c2291
David Heinemeier Hansson <dhh@hey.com>
2023-01-27 15:56:07 +0100

    Better error reporting and failure capture for build push

--- lib/mrsk/cli/build.rb
+++ lib/mrsk/cli/build.rb
@@ -10,15 +10,18 @@ class Mrsk::Cli::Build < Mrsk::Cli::Base
   desc "push", "Build locally and push app image to registry"
   def push
     verbose = options[:verbose]
+    cli = self
 
     run_locally do 
       begin
         MRSK.verbosity(:debug) { execute *MRSK.builder.push }
       rescue SSHKit::Command::Failed => e
-        if e.message =~ /no such file or directory/
+        if e.message =~ /(no builder)|(no such file or directory)/
           error "Missing compatible builder, so creating a new one first"
-          execute *MRSK.builder.create
-          MRSK.verbosity(:debug) { execute *MRSK.builder.push }
+
+          if cli.create
+            MRSK.verbosity(:debug) { execute *MRSK.builder.push }
+          end
         else
           raise
         end
@@ -34,8 +37,17 @@ class Mrsk::Cli::Build < Mrsk::Cli::Base
   desc "create", "Create a local build setup"
   def create
     run_locally do
-      debug "Using builder: #{MRSK.builder.name}"
-      execute *MRSK.builder.create
+      begin
+        debug "Using builder: #{MRSK.builder.name}"
+        execute *MRSK.builder.create
+      rescue SSHKit::Command::Failed => e
+        if e.message =~ /stderr=(.*)/
+          error "Couldn't create remote builder: #{$1}"
+          false
+        else
+          raise
+        end
+      end
     end
   end
 
