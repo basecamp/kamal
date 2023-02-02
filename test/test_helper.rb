288b272fe15101cdd384a7fac6632d1a7a4e9cee
Rob Zolkos <rob@zolkos.com>
2023-02-02 17:29:03 -0500

    Add test for Zeitwerk compliance
    This eager loads the files as per Zeitwerk
    [docs](https://github.com/fxn/zeitwerk#testing-compliance)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,10 +3,23 @@ require "active_support/test_case"
 require "active_support/testing/autorun"
 require "debug"
 require "sshkit"
+require "zeitwerk"
 
 ActiveSupport::LogSubscriber.logger = ActiveSupport::Logger.new(STDOUT) if ENV["VERBOSE"]
 
 SSHKit.config.backend = SSHKit::Backend::Printer
 
 class ActiveSupport::TestCase
+  test "Zeitwerk compliance" do 
+    loader = Zeitwerk::Loader.for_gem
+    loader.setup
+
+    begin
+      loader.eager_load(force: true)
+    rescue Zeitwerk::NameError => e
+      flunk e.message
+    else
+      assert true
+    end
+  end
 end
