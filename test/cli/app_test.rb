35b42cc8859bd94eeadc985a68c96d3f2dc41090
David Heinemeier Hansson <dhh@hey.com>
2023-02-02 18:05:56 +0100

    Fix tests

--- test/cli/app_test.rb
+++ test/cli/app_test.rb
@@ -2,14 +2,14 @@ require_relative "cli_test_case"
 
 class CliAppTest < CliTestCase
   test "boot" do
-    assert_match /Running docker run -d --restart unless-stopped --name app-999/, run_command("boot")
+    assert_match /Running docker run -d --restart unless-stopped/, run_command("boot")
   end
 
   test "reboot" do
     run_command("reboot").tap do |output|
       assert_match /docker stop/, output
       assert_match /docker container prune/, output
-      assert_match /docker run -d --restart unless-stopped --name app-999/, output
+      assert_match /docker run -d --restart unless-stopped/, output
     end
   end
 
