58417f610fc5c5983e8ad0395d9866124146e423
David Heinemeier Hansson <dhh@hey.com>
2023-02-03 17:20:14 +0100

    Dupe comment

--- test/cli/app_test.rb
+++ test/cli/app_test.rb
@@ -56,7 +56,7 @@ class CliAppTest < CliTestCase
   test "exec with reuse" do
     run_command("exec", "--reuse", "ruby -v").tap do |output|
       assert_match %r[docker ps --filter label=service=app --format \"{{.Names}}\" | sed 's/-/\\n/g' | tail -n 1], output # Get current version
-      assert_match %r[docker exec app-999 ruby -v], output # Get current version
+      assert_match %r[docker exec app-999 ruby -v], output
     end
   end
 
