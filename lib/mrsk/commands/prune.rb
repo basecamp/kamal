f25f506d77752fb4b7b41593219c16e0f8f2a74a
David Heinemeier Hansson <dhh@hey.com>
2023-02-15 13:26:57 +0100

    Don't use abbreviations when we don't have to

--- lib/mrsk/commands/prune.rb
+++ lib/mrsk/commands/prune.rb
@@ -6,11 +6,11 @@ class Mrsk::Commands::Prune < Mrsk::Commands::Base
   PRUNE_CONTAINERS_AFTER = 3.days.in_hours.to_i
 
   def images
-    docker :image, :prune, "-f", "--filter", "until=#{PRUNE_IMAGES_AFTER}h"
+    docker :image, :prune, "--force", "--filter", "until=#{PRUNE_IMAGES_AFTER}h"
   end
 
   def containers
-    docker :image, :prune, "-f", "--filter", "until=#{PRUNE_IMAGES_AFTER}h"
-    docker :container, :prune, "-f", "--filter", "label=service=#{config.service}", "--filter", "'until=#{PRUNE_CONTAINERS_AFTER}h'"
+    docker :image, :prune, "--force", "--filter", "until=#{PRUNE_IMAGES_AFTER}h"
+    docker :container, :prune, "--force", "--filter", "label=service=#{config.service}", "--filter", "'until=#{PRUNE_CONTAINERS_AFTER}h'"
   end
 end
