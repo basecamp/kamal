3457c3f606310e90c60fbf3007f0e570e55ffc49
David Heinemeier Hansson <dhh@hey.com>
2023-03-08 18:05:00 +0100

    Style

--- lib/mrsk/cli/server.rb
+++ lib/mrsk/cli/server.rb
@@ -2,13 +2,13 @@ class Mrsk::Cli::Server < Mrsk::Cli::Base
   desc "bootstrap", "Ensure curl and Docker are installed on servers"
   def bootstrap
     on(MRSK.hosts + MRSK.accessory_hosts) do
-      dependencies_to_install = [].tap do |dependencies|
+      dependencies_to_install = Array.new.tap do |dependencies|
         dependencies << "curl" unless execute "which curl", raise_on_non_zero_exit: false
         dependencies << "docker.io" unless execute "which docker", raise_on_non_zero_exit: false
       end
 
       if dependencies_to_install.any?
-        execute "apt-get update -y && apt-get install #{dependencies_to_install.join(' ')} -y)"
+        execute "apt-get update -y && apt-get install #{dependencies_to_install.join(" ")} -y)"
       end
     end
   end
