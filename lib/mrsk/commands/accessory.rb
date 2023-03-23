5d5d58a4ec0bd625cc5908b5e8690a9156a00ebb
Javier Aranda <aranda@hey.com>
2023-03-23 23:56:59 +0100

    #142 Allow to customize container options in accessories

--- lib/mrsk/commands/accessory.rb
+++ lib/mrsk/commands/accessory.rb
@@ -1,6 +1,7 @@
 class Mrsk::Commands::Accessory < Mrsk::Commands::Base
   attr_reader :accessory_config
-  delegate :service_name, :image, :host, :port, :files, :directories, :publish_args, :env_args, :volume_args, :label_args, to: :accessory_config
+  delegate :service_name, :image, :host, :port, :files, :directories, :publish_args, :env_args, :volume_args,
+           :label_args, :option_args, to: :accessory_config
 
   def initialize(config, name:)
     super(config)
@@ -17,6 +18,7 @@ class Mrsk::Commands::Accessory < Mrsk::Commands::Base
       *env_args,
       *volume_args,
       *label_args,
+      *option_args,
       image
   end
 
