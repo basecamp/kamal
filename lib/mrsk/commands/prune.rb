079d9538bbef4bc09e69ad77cdb8269c70522445
Donal McBreen <donal@basecamp.com>
2023-05-31 09:43:50 +0100

    Improve image pruning robustness
    If you different images with the same git SHA, on the second deploy the
    tag is moved and the first image becomes untagged. It may however still
    be attached to an existing container.
    
    To handle this:
    1. Initially prune dangling images - this will remove any untagged
    images that are not attached to an existing image
    2. Then filter out the untagged images when deleting tagged images - any
    that remain will be attached to a container.
    
    The second issue is that `docker container ls -a --format '{{.Image}}`
    will sometimes return the image id rather than a tag. This means that
    the image doesn't get filtered out when we grep to remove the active
    images.
    
    To fix that we'll grep against both the image id and repo:tag.

--- lib/mrsk/commands/prune.rb
+++ lib/mrsk/commands/prune.rb
@@ -2,11 +2,15 @@ require "active_support/duration"
 require "active_support/core_ext/numeric/time"
 
 class Mrsk::Commands::Prune < Mrsk::Commands::Base
-  def images
+  def dangling_images
+    docker :image, :prune, "--force", "--filter", "label=service=#{config.service}", "--filter", "dangling=true"
+  end
+
+  def tagged_images
     pipe \
-      docker(:image, :ls, *service_filter, "--format", "'{{.Repository}}:{{.Tag}}'"),
+      docker(:image, :ls, *service_filter, "--format", "'{{.ID}} {{.Repository}}:{{.Tag}}'"),
       "grep -v -w \"#{active_image_list}\"",
-      "while read tag; do docker rmi $tag; done"
+      "while read image tag; do docker rmi $tag; done"
   end
 
   def containers(keep_last: 5)
@@ -22,7 +26,10 @@ class Mrsk::Commands::Prune < Mrsk::Commands::Base
     end
 
     def active_image_list
-      "$(docker container ls -a --format '{{.Image}}\\|' --filter label=service=#{config.service} | tr -d '\\n')#{config.latest_image}"
+      # Pull the images that are used by any containers
+      # Append repo:latest - to avoid deleting the latest tag
+      # Append repo:<none> - to avoid deleting dangling images that are in use. Unused dangling images are deleted separately
+      "$(docker container ls -a --format '{{.Image}}\\|' --filter label=service=#{config.service} | tr -d '\\n')#{config.latest_image}\\|#{config.repository}:<none>"
     end
 
     def service_filter
