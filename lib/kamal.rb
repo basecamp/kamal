0c910f20e289c63a11405489be25b80f309ea2ff
Fran Zekan <zekan.fran369@gmail.com>
2023-08-27 17:29:46 +0200

    Load local bin/kamal if exists, fallback to running gem

--- lib/kamal.rb
+++ lib/kamal.rb
@@ -6,5 +6,6 @@ require "zeitwerk"
 
 loader = Zeitwerk::Loader.for_gem
 loader.ignore("#{__dir__}/kamal/sshkit_with_ext.rb")
+loader.ignore("#{__dir__}/bin_loader.rb")
 loader.setup
 loader.eager_load # We need all commands loaded.
