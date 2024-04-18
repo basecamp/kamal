76f03c8c9b9f76632b4ea927c0b03ac566ff81d4
swombat <daniel.github@tenner.org>
2024-04-18 11:07:18 +0100

    Oops typo

--- lib/kamal/configuration/builder.rb
+++ lib/kamal/configuration/builder.rb
@@ -39,10 +39,6 @@ class Kamal::Configuration::Builder
     @options["tag_uncommitted"] != false
   end
 
-  def 
-    @options["build_args"] || {}
-  end
-
   def dockerfile
     @options["dockerfile"] || "Dockerfile"
   end
