5d8e4dee13c465d77fba998630e0183d3c8dca52
Donal McBreen <donal@basecamp.com>
2024-06-06 12:07:20 +0100

    Create a context for local builds
    This ensures we use the docker-container driver and not whatever the
    local default is.

--- lib/kamal/commands/builder/local.rb
+++ lib/kamal/commands/builder/local.rb
@@ -1,8 +1,10 @@
 class Kamal::Commands::Builder::Local < Kamal::Commands::Builder::Base
   def create
+    docker :buildx, :create, "--name", builder_name, "--driver=docker-container"
   end
 
   def remove
+    docker :buildx, :rm, builder_name
   end
 
   def info
@@ -18,6 +20,10 @@ class Kamal::Commands::Builder::Local < Kamal::Commands::Builder::Base
       "--builder", builder_name,
       *build_options,
       build_context
+  end
+
+  def context_hosts
+    docker :buildx, :inspect, builder_name, "> /dev/null"
   end
 
   private
