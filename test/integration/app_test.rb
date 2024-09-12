109339189aa79be82fe2452ffca0afc14e7a2a20
Donal McBreen <donal@37signals.com>
2024-09-12 14:18:13 +0100

    Fix up integration app_test.rb

--- test/integration/app_test.rb
+++ test/integration/app_test.rb
@@ -20,30 +20,30 @@ class AppTest < IntegrationTest
     wait_for_app_to_be_up
 
     logs = kamal :app, :logs, capture: true
-    assert_match /App Host: vm1/, logs
-    assert_match /App Host: vm2/, logs
-    assert_match /GET \/ HTTP\/1.1/, logs
+    assert_match "App Host: vm1", logs
+    assert_match "App Host: vm2", logs
+    assert_match "GET /version HTTP/1.1", logs
 
     images = kamal :app, :images, capture: true
-    assert_match /App Host: vm1/, images
-    assert_match /App Host: vm2/, images
+    assert_match "App Host: vm1", images
+    assert_match "App Host: vm2", images
     assert_match /registry:4443\/app\s+#{latest_app_version}/, images
     assert_match /registry:4443\/app\s+latest/, images
 
     containers = kamal :app, :containers, capture: true
-    assert_match /App Host: vm1/, containers
-    assert_match /App Host: vm2/, containers
-    assert_match /registry:4443\/app:#{latest_app_version}/, containers
-    assert_match /registry:4443\/app:latest/, containers
+    assert_match "App Host: vm1", containers
+    assert_match "App Host: vm2", containers
+    assert_match "registry:4443/app:#{latest_app_version}", containers
+    assert_match "registry:4443/app:latest", containers
 
     exec_output = kamal :app, :exec, :ps, capture: true
-    assert_match /App Host: vm1/, exec_output
-    assert_match /App Host: vm2/, exec_output
+    assert_match "App Host: vm1", exec_output
+    assert_match "App Host: vm2", exec_output
     assert_match /1 root      0:\d\d ps/, exec_output
 
     exec_output = kamal :app, :exec, "--reuse", :ps, capture: true
-    assert_match /App Host: vm1/, exec_output
-    assert_match /App Host: vm2/, exec_output
+    assert_match "App Host: vm2", exec_output
+    assert_match "App Host: vm1", exec_output
     assert_match /1 root      0:\d\d nginx/, exec_output
 
     kamal :app, :remove
