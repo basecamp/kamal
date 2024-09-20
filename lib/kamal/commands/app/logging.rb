6ab5fc9459bd42af4bd4e0d3d09b58fc856cc0b3
David Heinemeier Hansson <david@hey.com>
2024-09-20 08:04:28 -0700

    Allow timestamps on/off for app logging too

--- lib/kamal/commands/app/logging.rb
+++ lib/kamal/commands/app/logging.rb
@@ -1,8 +1,8 @@
 module Kamal::Commands::App::Logging
-  def logs(version: nil, since: nil, lines: nil, grep: nil, grep_options: nil)
+  def logs(version: nil, timestamps: true, since: nil, lines: nil, grep: nil, grep_options: nil)
     pipe \
       version ? container_id_for_version(version) : current_running_container_id,
-      "xargs docker logs#{" --since #{since}" if since}#{" --tail #{lines}" if lines} 2>&1",
+      "xargs docker logs#{" --timestamps" if timestamps}#{" --since #{since}" if since}#{" --tail #{lines}" if lines} 2>&1",
       ("grep '#{grep}'#{" #{grep_options}" if grep_options}" if grep)
   end
 
@@ -10,7 +10,7 @@ module Kamal::Commands::App::Logging
     run_over_ssh \
       pipe(
         current_running_container_id,
-        "xargs docker logs #{"--timestamps" if timestamps}#{" --tail #{lines}" if lines} --follow 2>&1",
+        "xargs docker logs#{" --timestamps" if timestamps}#{" --tail #{lines}" if lines} --follow 2>&1",
         (%(grep "#{grep}"#{" #{grep_options}" if grep_options}) if grep)
       ),
       host: host
