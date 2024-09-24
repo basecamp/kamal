874053bda2a402710cb48bdaef6aa69e20cc8bd5
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2024-09-24 14:39:52 -0400

    Added support for custom SSL certificate

--- lib/kamal/configuration/validator/proxy.rb
+++ lib/kamal/configuration/validator/proxy.rb
@@ -10,6 +10,14 @@ class Kamal::Configuration::Validator::Proxy < Kamal::Configuration::Validator
       if (config.keys & [ "host", "hosts" ]).size > 1
         error "Specify one of 'host' or 'hosts', not both"
       end
+
+      if config["ssl_certificate_path"].present? && config["ssl_private_key_path"].blank?
+        error "Must set a private key path to use a custom SSL certificate"
+      end
+
+      if config["ssl_private_key_path"].present? && config["ssl_certificate_path"].blank?
+        error "Must set a certificate path to use a custom SSL private key"
+      end
     end
   end
 end
