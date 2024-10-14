7d6fe5c13072ceb6b2c718fe1627bc9fbff57f63
Alan Oliveira <alan.oliveira89@gmail.com>
2024-10-14 20:22:02 +0900

    remove shellescaping from secrets fetch output

--- test/cli/secrets_test.rb
+++ test/cli/secrets_test.rb
@@ -3,7 +3,7 @@ require_relative "cli_test_case"
 class CliSecretsTest < CliTestCase
   test "fetch" do
     assert_equal \
-      "\\{\\\"foo\\\":\\\"oof\\\",\\\"bar\\\":\\\"rab\\\",\\\"baz\\\":\\\"zab\\\"\\}",
+      "{\"foo\":\"oof\",\"bar\":\"rab\",\"baz\":\"zab\"}",
       run_command("fetch", "foo", "bar", "baz", "--account", "myaccount", "--adapter", "test")
   end
 
@@ -13,6 +13,11 @@ class CliSecretsTest < CliTestCase
 
   test "extract match from end" do
     assert_equal "oof", run_command("extract", "foo", "{\"abc/foo\":\"oof\", \"bar\":\"rab\", \"baz\":\"zab\"}")
+  end
+
+  test "extract from fetched" do
+    fetched = run_command("fetch", "foo", "bar", "baz", "--account", "myaccount", "--adapter", "test")
+    assert_equal "oof", run_command("extract", "foo", fetched)
   end
 
   test "print" do
