8dd864af89e8e6875a783357310a4200b4a014ae
Ralf Schmitz Bongiolo <mrbongiolo@hey.com>
2024-11-05 14:14:18 -0400

    refactor(secrets): adapter/test_optional_account inherit from adapter/test

--- lib/kamal/secrets/adapters/test_optional_account.rb
+++ lib/kamal/secrets/adapters/test_optional_account.rb
@@ -1,18 +1,5 @@
-class Kamal::Secrets::Adapters::TestOptionalAccount < Kamal::Secrets::Adapters::Base
+class Kamal::Secrets::Adapters::TestOptionalAccount < Kamal::Secrets::Adapters::Test
   def requires_account?
     false
   end
-
-  private
-    def login(account)
-      true
-    end
-
-    def fetch_secrets(secrets, account:, session:)
-      secrets.to_h { |secret| [ secret, secret.reverse ] }
-    end
-
-    def check_dependencies!
-      # no op
-    end
 end
