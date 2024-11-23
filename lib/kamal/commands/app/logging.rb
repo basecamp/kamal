1cc5406b00048775dce4b75aabd94512ee821abb
Ali Ismayilov <993934+aliismayilov@users.noreply.github.com>
2024-11-23 21:37:58 +0100

    Pipe app container id

--- lib/kamal/commands/app/logging.rb
+++ lib/kamal/commands/app/logging.rb
@@ -1,7 +1,7 @@
 module Kamal::Commands::App::Logging
   def logs(container_id: nil, timestamps: true, since: nil, lines: nil, grep: nil, grep_options: nil)
     pipe \
-      container_id ? "echo #{container_id}" : current_running_container_id,
+      container_id_command(container_id),
       "xargs docker logs#{" --timestamps" if timestamps}#{" --since #{since}" if since}#{" --tail #{lines}" if lines} 2>&1",
       ("grep '#{grep}'#{" #{grep_options}" if grep_options}" if grep)
   end
@@ -9,10 +9,20 @@ module Kamal::Commands::App::Logging
   def follow_logs(host:, container_id: nil, timestamps: true, lines: nil, grep: nil, grep_options: nil)
     run_over_ssh \
       pipe(
-        container_id ? "echo #{container_id}" : current_running_container_id,
+        container_id_command(container_id),
         "xargs docker logs#{" --timestamps" if timestamps}#{" --tail #{lines}" if lines} --follow 2>&1",
         (%(grep "#{grep}"#{" #{grep_options}" if grep_options}) if grep)
       ),
       host: host
+  end
+
+  private
+
+  def container_id_command(container_id)
+    case container_id
+    when Array then container_id
+    when String, Symbol then "echo #{container_id}"
+    else current_running_container_id
+    end
   end
 end
