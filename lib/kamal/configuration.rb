16fb3adacb70d600bfbe2136c76905960d978349
Jeremy Daer <jeremy@37signals.com>
2024-12-10 16:08:56 -0800

    No need for IO.read for basic file paths
    References 3cad095, e1d5182

--- lib/kamal/configuration.rb
+++ lib/kamal/configuration.rb
@@ -37,7 +37,7 @@ class Kamal::Configuration
         if file.exist?
           # Newer Psych doesn't load aliases by default
           load_method = YAML.respond_to?(:unsafe_load) ? :unsafe_load : :load
-          YAML.send(load_method, ERB.new(IO.read(file)).result).symbolize_keys
+          YAML.send(load_method, ERB.new(File.read(file)).result).symbolize_keys
         else
           raise "Configuration file not found in #{file}"
         end
