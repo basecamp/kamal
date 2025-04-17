55ec6ca0a6b43b925d520195881298d89b5d0ab4
Donal McBreen <donal@37signals.com>
2025-04-17 11:40:03 +0100

    Allow accessory only configurations
    If there are accessories defined in the configuration, we'll not require
    servers to be defined as well.
    
    This allows for accessory-only configurations which allows you to run
    external images with kamal-proxy for zero-downtime deployments.
    
    We don't manage image cleanup for accessories though so the user will
    need to deal with that themselves.

--- test/integration/accessory_test.rb
+++ test/integration/accessory_test.rb
@@ -21,6 +21,32 @@ class AccessoryTest < IntegrationTest
     assert_accessory_not_running :busybox
   end
 
+  test "proxied: boot, stop, start, restart, logs, remove" do
+    @app = "app_with_proxied_accessory"
+
+    kamal :proxy, :boot
+
+    kamal :accessory, :boot, :netcat
+    assert_accessory_running :netcat
+    assert_netcat_is_up
+
+    kamal :accessory, :stop, :netcat
+    assert_accessory_not_running :netcat
+    assert_netcat_not_found
+
+    kamal :accessory, :start, :netcat
+    assert_accessory_running :netcat
+    assert_netcat_is_up
+
+    kamal :accessory, :restart, :netcat
+    assert_accessory_running :netcat
+    assert_netcat_is_up
+
+    kamal :accessory, :remove, :netcat, "-y"
+    assert_accessory_not_running :netcat
+    assert_netcat_not_found
+  end
+
   private
     def assert_accessory_running(name)
       assert_match /registry:4443\/busybox:1.36.0   "sh -c 'echo \\"Start/, accessory_details(name)
@@ -32,5 +58,26 @@ class AccessoryTest < IntegrationTest
 
     def accessory_details(name)
       kamal :accessory, :details, name, capture: true
+    end
+
+    def assert_netcat_is_up
+      response = netcat_response
+      debug_response_code(response, "200")
+      assert_equal "200", response.code
+    end
+
+    def assert_netcat_not_found
+      response = netcat_response
+      debug_response_code(response, "404")
+      assert_equal "404", response.code
+    end
+
+    def netcat_response
+      uri = URI.parse("http://127.0.0.1:12345/up")
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Get.new(uri)
+      request["Host"] = "netcat"
+
+      http.request(request)
     end
 end
