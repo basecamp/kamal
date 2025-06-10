1d88281fee8bebcd522dc1d8600dd76348755c1c
Nick Hammond <nick@nickhammond.com>
2025-06-09 19:08:20 -0700

    Validate that destination, role, and service are not set as labels on roles and accessories

--- lib/kamal/configuration/validator/role.rb
+++ lib/kamal/configuration/validator/role.rb
@@ -2,6 +2,8 @@ class Kamal::Configuration::Validator::Role < Kamal::Configuration::Validator
   def validate!
     validate_type! config, Array, Hash
 
+    validate_labels!(config["labels"]) if config.key?("labels")
+
     if config.is_a?(Array)
       validate_servers!(config)
     else
