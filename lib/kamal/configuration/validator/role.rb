6672e3e77dc748bdd934493685b550c1d603c0a9
Nick Hammond <nick@nickhammond.com>
2025-06-09 19:47:26 -0700

    Remove blank line

--- lib/kamal/configuration/validator/role.rb
+++ lib/kamal/configuration/validator/role.rb
@@ -2,7 +2,6 @@ class Kamal::Configuration::Validator::Role < Kamal::Configuration::Validator
   def validate!
     validate_type! config, Array, Hash
 
-
     if config.is_a?(Array)
       validate_servers!(config)
     else
